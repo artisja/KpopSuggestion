1354578f1e738f5a766de5bf56a4820346470b94
artisja <artisja@vcu.edu>
2017-05-28 15:40:54 -0400

    Added image picker functionality along with firebase storage capabilities

--- app/src/main/java/com/example/artisja/kpopsuggestion/AddSongActivity.java
+++ app/src/main/java/com/example/artisja/kpopsuggestion/AddSongActivity.java
@@ -2,23 +2,29 @@ package com.example.artisja.kpopsuggestion;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.provider.MediaStore;
-import android.support.v4.content.ContextCompat;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.SecureRandom;
 
 import co.mobiwise.materialintro.shape.Focus;
@@ -29,12 +35,16 @@ import co.mobiwise.materialintro.view.MaterialIntroView;
 
 public class AddSongActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 111;
     InputRow nameRow,artistRow,albumRow,urlRow;
     Button submitButton,imagePicker;
     private static final String SHOWCASE_ID = "995";
-    private static final Integer IMAGE_PICK = 12345;
+    private static final int IMAGE_PICK = 12345;
     public DatabaseReference firebaseDatabase;
-
+    public Bitmap bitmap;
+    public FirebaseStorage firebaseStorage;
+    public StorageReference storageReference;
+    private Uri imagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +72,8 @@ public class AddSongActivity extends AppCompatActivity {
 
     private void setUpDatabase() {
         firebaseDatabase = FirebaseDatabase.getInstance().getReference();
-
+        firebaseStorage  = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReferenceFromUrl("gs://kpopsuggestionapp.appspot.com/");
     }
 
     private void setUpViews() {
@@ -83,9 +94,10 @@ public class AddSongActivity extends AppCompatActivity {
         imagePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                getIntent.setType("image/*");
-                startActivityForResult(Intent.createChooser(getIntent,"Select Picture"), IMAGE_PICK);
+                Intent getImageIntent = new Intent();
+                getImageIntent.setType("image/*");
+                getImageIntent.setAction(Intent.ACTION_PICK);
+                startActivityForResult(Intent.createChooser(getImageIntent,"Select Image"),PICK_IMAGE_REQUEST);
             }
         });
 
@@ -97,30 +109,51 @@ public class AddSongActivity extends AppCompatActivity {
             public void onClick(View v) {
                 SecureRandom secureRandom = new SecureRandom();
                 int id = secureRandom.nextInt();
-                SongInfo song = new SongInfo(nameRow.getInputEdit(),artistRow.getInputEdit(),albumRow.getInputEdit(),urlRow.getInputEdit());
-                firebaseDatabase.child(String.valueOf(id)).setValue(song);
+                addToDatabase(id);
+                addToStorage(id);
                 startActivity(intent);
-                Toast.makeText(AddSongActivity.this, "Added to Google Firebase", Toast.LENGTH_SHORT).show();
 
             }
         });
     }
 
+    private void addToStorage(int id) {
+        StorageReference imageRef = storageReference.child( String.valueOf(id)+".jpg");
+        UploadTask uploadTask = imageRef.putFile(imagePath);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(AddSongActivity.this, "Successful Upload", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(AddSongActivity.this, "Upload Failure", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void addToDatabase(int id) {
+        SongInfo song = new SongInfo(nameRow.getInputEdit(),artistRow.getInputEdit(),albumRow.getInputEdit(),urlRow.getInputEdit());
+        firebaseDatabase.child(String.valueOf(id)).setValue(song);
+        Toast.makeText(AddSongActivity.this, "Added to Google Firebase", Toast.LENGTH_SHORT).show();
+    }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == IMAGE_PICK && resultCode == RESULT_OK && data.getData() !=null){
-            Uri image = data.getData();
-            try{
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),image);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data.getData() !=null){
+                imagePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imagePath);
                 ImageView imageView = (ImageView) findViewById(R.id.test_image);
                 imageView.setImageBitmap(bitmap);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
         }
     }
 }
